fc476c3961d3f0289430824401e1844fdaa1290a
Andre Quispesaravia <andreq@fb.com>
2018-05-07 21:55:16 -0700

    [selfiemash] create routes with express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/hello', (req, res) => res.send('Hello World!'));
+app.get('/summary', (req, res) => res.send('Summary!'));
 
-server.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
